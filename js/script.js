2b1f1110814b01efea1715b0bbe66d12deaf48d2
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-09 04:51:09 +0000

    ok

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-// Copyright (c) 2022 Emmanuel-Fofeyin All rights reserved
+// Copyright (c) 2022 Brayden Blank All rights reserved
 //
-// Created by: Emmanuel-Fofeyin
+// Created by: Brayden Blank
 // Created on: Apr 2022
 // This file contains the JS functions for index.html
 
-"use strict"
+"use strict";
 
 /**
  * Check servie worker.
@@ -12,25 +12,26 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Unit-5-04-HTML/sw.js", {
     scope: "/ICS2O-Unit-5-04-HTML/",
-  })
+  });
 }
 
 /**
  * This function compares slider with random number.
  */
 function guessClicked() {
-  var date = document.getElementById("date").value
-  var userAge = parseInt(document.getElementById("user-age").value)
+  var date = document.getElementById("date").value;
+  var userAge = parseInt(document.getElementById("user-age").value);
 
-  if ((    
-    date == "tuesday" ||
-    date == "thursday") ||
-      (userAge > 12 && userAge < 21)){
+  if ((date == "tuesday" || date == "thursday") || (userAge > 12 && userAge < 21)) {
     document.getElementById("guess-result").innerHTML = 
-      "You are eligible for a discount!"
+      "You are eligible for a discount!";
   }
   else {
-    document.getElementById("guess-result").innerHTML =
-      "You are not eligible for a discount."
+    document.getElementById("guess-result").innerHTML = 
+      "You are not eligible for a discount.";
+  }
+}
+    document.getElementById("guess-result").innerHTML = 
+      "You are not eligible for a discount.";
   }
 }
