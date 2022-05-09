3ec5ae96cd2a8d01a0b9972b01ec618d876c04a1
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-09 04:14:24 +0000

    rest

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-// Copyright (c) 2022 Emmanuel-Fofeyin All rights reserved
+// Copyright (c) 2022 Brayden Blank All rights reserved
 //
-// Created by: Emmanuel-Fofeyin
+// Created by: Brayden Blank
 // Created on: Apr 2022
 // This file contains the JS functions for index.html
 
-"use strict"
+"use strict";
 
 /**
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-04-HTML/sw.js", {
-    scope: "/ICS2O-Unit5-04-HTML/",
-  })
+  navigator.serviceWorker.register("/ICS2O-Unit-5-04-HTML/sw.js", {
+    scope: "/ICS2O-Unit-5-04-HTML/",
+  });
 }
 
 /**
@@ -23,11 +23,11 @@ function guessClicked() {
   var userAge = parseInt(document.getElementById("user-age").value)
 
   if ((date == "tuesday" || date == "thursday") || (userAge > 12 && userAge < 21)) {
-    document.getElementById("guess-result").innerHTML =
+    document.getElementById("guess-result").innerHTML = 
       "You are eligible for a discount!"
   }
   else {
-    document.getElementById("guess-result").innerHTML =
+    document.getElementById("guess-result").innerHTML = 
       "You are not eligible for a discount."
   }
 }
